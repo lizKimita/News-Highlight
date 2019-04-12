20ad0d5f823e879ab9a75f0390db491c51d28cac
lizKimita <kimita.wanjiku@gmail.com>
2019-04-12 12:39:18 +0300

    adding the get and process functions for the sources in the request file

--- app/request.py
+++ app/request.py
@@ -1,4 +1,56 @@
 from app import app
+import urllib.request,json
+from .models import source
+
+Source = source.Source
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the news base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['results']:
+            source_results_list = get_sources_response['results']
+            source_results = process_results(source_results_list)
+
+    return source_results
+
+def process_results(source_list):
+    '''
+    Function  that processes the source result and transform them to a list of Objects
+
+    Args:
+        source_list: A list of dictionaries that contain the source details
+
+    Returns :
+        source_results: A list of source objects
+    '''
+    source_results = []
+
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        category = source_item.get('category')
+        country = source_item.get('country')
+        url = source_item.get('url')
+
+        if url:
+            source_object = Source(id, name, description, category, country, url)
+            source_results.append(source_object)
+
+    return source_results
